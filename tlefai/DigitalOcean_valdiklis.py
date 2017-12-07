a12baf3e3fae0c2825215990dc8186608b088479
Saimonas <outoflock377@gmail.com>
2017-12-07 16:38:18 +0200

    Added control for DigitalOcean

---
+++ tlefai/DigitalOcean_valdiklis.py
@@ -0,0 +1,13 @@
+def patikrinti_api_key():
+    return True
+
+def generuoti_ssh_raktus():
+    return True
+
+def ideti_API_rakta():
+    return True
+
+def ideti_SSH_raktus():
+    return True
+
+
