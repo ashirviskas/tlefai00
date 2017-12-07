a5af74571d23ad5b0cdbe0cbbe7a89e56d01413a
tlef00 <minematas@gmail.com>
2017-12-07 17:20:29 +0200

    Forgot to add file.

---
+++ tlefai/kliento_registracijos_valdiklis.py
@@ -0,0 +1,13 @@
+from time import gmtime, strftime
+def uzregistruoti_vartotoja(db, username, email, password):
+    cur = db.cursor()
+    try:
+        cur.execute("""INSERT INTO User (username, email, password, date_of_signup) VALUES (%s,%s,%s,%s)""",(username, email, password, strftime("%Y-%m-%d %H:%M:%S", gmtime())))
+        db.commit()
+    except:
+        print("Failed adding to database")
+        return False
+    return True
+
+def prisijungti(db, username):
+    return 'Prijungto vartotojo puslapis, vartotojas: ' + username
