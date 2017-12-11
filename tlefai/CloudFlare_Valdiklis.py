ad980a957977dd30f758b0ccb3794cd746443484
Lukas <lukas.strav@gmail.com>
2017-12-11 14:55:04 +0200

    api check

--- tlefai/CloudFlare_Valdiklis.py
+++ tlefai/CloudFlare_Valdiklis.py
@@ -1,4 +1,17 @@
-def patikrinti_api_key():
+import requests
+base_link="https://api.cloudflare.com/client/v4/user/"
+def patikrinti_api_key(email, api_key):
+
+    parameters = {"X-Auth-Email":email, "X-Auth-Key":api_key,  "Content-Type":"application/json", "first_name":"John"}
+
+    response = requests.request("PATCH", base_link, headers=parameters)
+    print(response.text)
+    if response.status_code == 200:
+        print("Authorisation successful")
+        return True
+    else:
+        print("Authorisation error")
+        return False
     return
 def sudeti_raktus_i_lentele():
     return
